966a01f3087acde0e9203ed4f9d93e43d7b157a6
Svibex <77160861+Svibex@users.noreply.github.com>
2021-03-14 21:41:29 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,40 +1,20 @@
 module.exports = function toReadable (number) {
-    const strings = {
-        a1: ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
-        a10: ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
-        a20: ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'],
-        a100: 'hundred'
-    };
-
-    const arr = `${number}`.split('').map(el => +el);
-    switch (arr.length) {
-        case 1: {
-            return strings.a1[number];
-        }
-        case 2: {
-            if (number < 20) {
-                return strings.a10[arr[1]];
-            } else {
-                return arr[1] === 0 ? strings.a20[arr[0] - 2] : `${strings.a20[arr[0] - 2]} ${strings.a1[arr[1]]}`;
-            }
-        }
-        case 3: {
-            if (!arr[1] && !arr[2]) {
-                return `${strings.a1[arr[0]]} ${strings.a100}`;
-            } else if (!arr[1]) {
-                return `${strings.a1[arr[0]]} ${strings.a100} ${strings.a1[arr[2]]}`;
-            } else if (!arr[2]) {
-                return arr[1] === 1
-                    ? `${strings.a1[arr[0]]} ${strings.a100} ${strings.a10[arr[1] - 1]}`
-                    : `${strings.a1[arr[0]]} ${strings.a100} ${strings.a20[arr[1] - 2]}`;
-            } else {
-                return arr[1] === 1
-                    ? `${strings.a1[arr[0]]} ${strings.a100} ${strings.a10[arr[2]]}`
-                    : `${strings.a1[arr[0]]} ${strings.a100} ${strings.a20[arr[1] - 2]} ${strings.a1[arr[2]]}`;
-            }
-        }
-        default: {
-            return null;
-        }
+    const zero = ['zero'];
+    const numbersFrom0To19 = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine','ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['','','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const hundred = ['hundred'];
+    if (number === 0) {
+        return zero;
+    } else if(number<20) {
+        return numbersFrom0To19[number];
+    } else if (number<100) {
+        return tensToString(number);
+    } else if (number>99) {
+        return numbersFrom0To19[Math.floor(number/100)] + ' ' + hundred + (number%100 ? ' ' + tensToString(number%100) : '');
+    }
+    function tensToString (num) {
+        return (num<20)
+            ? numbersFrom0To19[num]
+            : tens[Math.floor(num/10)] + (num%10 ?  ${numbersFrom0To19[num%10]} : '');
     }
 };
